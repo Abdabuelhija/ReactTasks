8c2d9f9a0405aa2e8743148b508b87fe6ed7a4a2
Abd abuelhija <a.abd.ab@hotmail.com>
2023-03-27 13:15:59 +0300

    first commit

---
+++ Task1.js
@@ -0,0 +1,67 @@
+// Abd abuelhija's solution
+
+// Q1
+// [10, 30, 20]
+
+// Q2
+var obj = {
+  numbers: {
+    a: 1,
+    b: 2
+  }
+};
+const { a, b } = obj.numbers;
+
+// Q3
+var arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+// Q4
+const { [propName]: name, age } = person;
+
+// Q5
+const { address: { street, city, state } } = person[propName];
+
+// Q6
+// const { 
+//   firstName,
+//   lastName,
+//   [propName]: { street, city, state = 'CA' } = {}
+// } = person;
+
+// Q7
+
+// const {
+//   name,
+//   age,
+//   address: {
+//     street,
+//     city,
+//     state,
+//     coordinates: { latitude, longitude }
+//   },
+//   getFullName
+// } = person;
+
+// Q8
+const [
+  { firstName: first = 'Unknown', lastName: last, age: years = 'Unknown' },
+  { firstName: fn = 'Unknown', lastName: ln = 'Unknown', age: yrs = 'Unknown' }
+] = people;
+
+// Q9
+// const { [propName]: { name, age, getAddress } } = obj;
+// const { street, city, state } = getAddress();
+
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Abdabuelhija/ReactTasks.git
+git push -u origin main
+
+
+
+
+
---
+++ Task3.js
@@ -0,0 +1,60 @@
+// Abd abuelhija's solution
+
+// Q1
+// [10, 30, 20]
+
+// Q2
+var obj = {
+  numbers: {
+    a: 1,
+    b: 2
+  }
+};
+const { a, b } = obj.numbers;
+
+// Q3
+var arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+// Q4
+const { [propName]: name, age } = person;
+
+// Q5
+const { address: { street, city, state } } = person[propName];
+
+// Q6
+// const { 
+//   firstName,
+//   lastName,
+//   [propName]: { street, city, state = 'CA' } = {}
+// } = person;
+
+// Q7
+
+// const {
+//   name,
+//   age,
+//   address: {
+//     street,
+//     city,
+//     state,
+//     coordinates: { latitude, longitude }
+//   },
+//   getFullName
+// } = person;
+
+// Q8
+const [
+  { firstName: first = 'Unknown', lastName: last, age: years = 'Unknown' },
+  { firstName: fn = 'Unknown', lastName: ln = 'Unknown', age: yrs = 'Unknown' }
+] = people;
+
+// Q9
+// const { [propName]: { name, age, getAddress } } = obj;
+// const { street, city, state } = getAddress();
+
+
+
+
+
+
